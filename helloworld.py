c879c533f0a2728e9011839812b7b0dc975e8b32
KitaeKim <rlarlxo84@gmail.com>
2025-01-15 15:56:06 +0900

    Upload test files

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+print(5) # 5 출력
+print(-10) # -10 출력
+print(3.14) # 3.14 출력
+print(1000) # 1000 출력
+print('풍선') # 작은 따옴표로 감싼 문자열 
+print("나비") # 큰 따옴표로 감싼 문자열 
+print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ") # 긴 문자열 
+print("ㅋ"*9) # ㅋ 을 9번 출력 
+print(5 > 10) # False 출력 
+print(5 < 10) # True 출력 
+print(True) # True 
+print(False) # False 
+print(not True) # False 출력 
+print(not False) # True 출력 
+print(not (5 > 10)) # True 출력 
---
+++ test.py
@@ -0,0 +1,51 @@
+# An Informal Introduction to Python
+
+
+# 주석사용법을 적어 놓은 것으로 보인다.
+
+spam = 1 # and this is the second comment
+         # ... and now a third!
+text = "# This is not a comment because it's inside quotes."
+
+print(2 + 2)
+# 4
+print(50 - 5*6)
+# 20
+print((50 - 5*6) / 4)
+# 5.0
+print(8 / 5) # 나누기는 언제나 float형으로 반환한다
+# 1.6
+
+
+print(17 / 3) # classic division returns a float 
+# 5.666666666666667
+print(17 // 3) # floor division discards the fractional part
+# 5 
+print(17 % 3) # operator returns the remainder of the division
+# 2
+print(5 * 3 + 2) #floored quotient * divisor + remainder
+# 17
+
+
+print(5 ** 2) # 5 squared, 5의 제곱을 표현
+# 25
+print(2 ** 7) # 2 to the power of 7
+# 128
+
+
+width = 20
+height = 5 * 9
+print(width * height)
+# 900
+
+n
+#Traceback (most recent call last):#  File "/Users/dream/PythonWorkspace/tempCodeRunnerFile.py", line 1, in <module>
+#    n
+#NameError: name 'n' is not defined
+
+print(4 * 3.75 - 1)
+# 14.0
+
+
+
+
