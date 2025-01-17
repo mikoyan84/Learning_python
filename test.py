6629bde064a5d8322aed5b4a76959b8a7c9ef7bd
KitaeKim <rlarlxo84@gmail.com>
2025-01-17 09:57:23 +0900

    blank

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 # An Informal Introduction to Python
 
-
 # 주석사용법을 적어 놓은 것으로 보인다.
 
 spam = 1 # and this is the second comment
@@ -60,9 +59,6 @@ print(round((price + price * tax), 2))
 # >>> 인터렉티브 모드에서 실행방법
 
 
-
-
-
 print('spam eggs')
 print("Paris rabbit got your back :)! Yay!")
 print('1975')
@@ -94,7 +90,54 @@ print('Py' 'thon')
 
 
 
-# plus로 해줘야
+# 변수끼리 혹은 변수와 문자열 리터럴(literal)을 붙이려면 + 를 사용해야 함
+# 변수와 문자열을 이어붙임
 
 prefix = 'Py'
 print(prefix + 'thon')
+
+
+# 인덱스 사용방법 
+word = 'Python'
+print(word[0]) # 변수 처음 문자인 P를 출력
+print(word[5]) # 변수 마지막 문자인 n을 출력
+
+# 인덱스는 음수가 될 수도 있다
+word = 'Python'
+print(word[-1]) # 변수 맨 마지막 n
+print(word[-2]) # 변수 맨 마지막 앞 o
+print(word[-6]) # 변수 맨 처음 P
+
+word = 'Python'
+print(word[:2] + word [2:]) # index 2까지 출력 index 2뒷부분 출력
+print(word[:4] + word [4:]) # index 2까지 출력 index 2뒷부분 출력
+
+#  +---+---+---+---+---+---+
+#  | P | y | t | h | o | n |
+#  +---+---+---+---+---+---+
+#  0   1   2   3   4   5   6
+# -6  -5  -4  -3  -2  -1
+
+word = 'Python'
+# print(word[4:42])
+print(word[42:])
+# 너무 큰 값을 인덱스로 사용하면 애러가 발생
+print(word[42])
+
+
+# 파이썬 문자열은 변경이 불가능하므로 다른 변수를 대입하려고 하면 에러를 발생시킨다.
+print(word[0] = 'J')
+
+word = 'Python'
+print( 'J' + word[1:])
+print(word[:2] + 'py')
+
+s = 'supercalifragilisticexpialidocious'
+print(len(s)) # len()는 문자열의 길이를 돌려준다
+
+
+
+
+
+
+
