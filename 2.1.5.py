b6ef2ee9405780e17fc9540983a5bc0954e65258
KitaeKim <rlarlxo84@gmail.com>
2025-01-23 09:18:07 +0900

    add files

---
+++ 2.1.5.py
@@ -0,0 +1,22 @@
+#(2𝑥−1)2+1
+#x=input()
+#print(x)
+#print(((2 * x) ** 2 - 1) + 1)
+
+
+
+#x=input()
+#print(((int(2) * (int(x)) - int(1)) ** int(2)) - int(1)) 
+
+
+#x=input()
+#print(((2 * (int(x)) - 1) ** 2) - 1) 
+
+
+
+j=input()
+k=input()
+i=input()
+print(((j == 0) & (0 < k)) | (i <= 100))
+
+
